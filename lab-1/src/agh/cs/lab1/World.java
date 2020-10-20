61386f6f4f1c7bf7c6e7dbf3e3622de316488b89
lstachon <lukaszstachon13@gmail.com>
2020-10-20 02:06:27 +0200

    testy manualne MapDirection

--- lab-1/src/agh/cs/lab1/World.java
+++ lab-1/src/agh/cs/lab1/World.java
@@ -45,39 +45,49 @@ public class World {
 
     public static void main(String[] args){
     System.out.println("start");
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
-    Scanner sc = new Scanner(System.in);
-    String str = sc.next();
-    int len = str.length()/2+1;
-    Direction moves[] = new Direction[len];
-    int j=0;
-    for(int i=0; i<str.length(); i++){
-        char c = str.charAt(i);
-                    switch(c){
-                case 'f':
-                    moves[j]=Direction.Forward;
-                    j++;
-                    break;
-                case 'b':
-                    moves[j]=Direction.Backward;
-                    j++;
-                    break;
-                case 'l':
-                    moves[j]=Direction.Left;
-                    j++;
-                    break;
-                case 'r':
-                    moves[j]=Direction.Right;
-                    j++;
-                    break;
-            }
 
-    }
-    World.run(moves);
+//manualne testy MapDirection
+    System.out.println(MapDirection.NORTH.next());
+    System.out.println(MapDirection.SOUTH.previous());
+    System.out.println(MapDirection.EAST.toUnitVector());
+    System.out.println(MapDirection.WEST.tostring());
+
+
+
+
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
+//    Scanner sc = new Scanner(System.in);
+//    String str = sc.next();
+//    int len = str.length()/2+1;
+//    Direction moves[] = new Direction[len];
+//    int j=0;
+//    for(int i=0; i<str.length(); i++){
+//        char c = str.charAt(i);
+//                    switch(c){
+//                case 'f':
+//                    moves[j]=Direction.Forward;
+//                    j++;
+//                    break;
+//                case 'b':
+//                    moves[j]=Direction.Backward;
+//                    j++;
+//                    break;
+//                case 'l':
+//                    moves[j]=Direction.Left;
+//                    j++;
+//                    break;
+//                case 'r':
+//                    moves[j]=Direction.Right;
+//                    j++;
+//                    break;
+//            }
+//
+//    }
+//    World.run(moves);
 
     System.out.println("end");
 
