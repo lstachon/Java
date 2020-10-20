3033ed3a2d8c6a6ee2bd44e7bbf26960bfb9ccf0
lstachon <lukaszstachon13@gmail.com>
2020-10-20 14:15:11 +0200

    mapdirection Tests Complited

--- lab-1/src/test/MapDirectionTest.java
+++ lab-1/src/test/MapDirectionTest.java
@@ -7,16 +7,52 @@ import static junit.framework.TestCase.assertEquals;
 public class MapDirectionTest{
 
     @Test
-    public  void Northn(){
-        MapDirection dir = MapDirection.NORTH;
-        assertEquals(dir.next(), MapDirection.EAST);
+    public  void nextNorth(){
+        MapDirection d = MapDirection.NORTH;
+        assertEquals(d.next(), MapDirection.EAST);
     }
     @Test
-    public void Southn(){
-        MapDirection dir = MapDirection.SOUTH;
-        assertEquals(dir.next(), MapDirection.WEST);
+    public void nextSouth(){
+        MapDirection d = MapDirection.SOUTH;
+        assertEquals(d.next(), MapDirection.WEST);
 
     }
+
+    @Test
+    public void nextEast(){
+        MapDirection d = MapDirection.EAST;
+        assertEquals(d.next(), MapDirection.SOUTH);
+    }
+    @Test
+    public void nextWest(){
+        MapDirection dir = MapDirection.WEST;
+        assertEquals(dir.next(), MapDirection.NORTH);
+    }
+
+
+    @Test
+    public  void prevNorth(){
+        MapDirection d = MapDirection.NORTH;
+        assertEquals(d.previous(), MapDirection.WEST);
+    }
+    @Test
+    public void prevSouth(){
+        MapDirection d = MapDirection.SOUTH;
+        assertEquals(d.previous(), MapDirection.EAST);
+
+    }
+
+    @Test
+    public void prevEast(){
+        MapDirection d = MapDirection.EAST;
+        assertEquals(d.previous(), MapDirection.NORTH);
+    }
+    @Test
+    public void prevWest(){
+        MapDirection dir = MapDirection.WEST;
+        assertEquals(dir.previous(), MapDirection.SOUTH);
+    }
+
 
 
 }
